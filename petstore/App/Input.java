7dbb6ec2aee95b3491dce9440cc591ce80e6a7e7
Grant Peverett <grantpeverett@gmail.com>
2025-03-27 20:21:00 -0500

    Java docs added to Input.java

--- petstore/App/Input.java
+++ petstore/App/Input.java
@@ -5,20 +5,31 @@ import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 /**
+ * Provides methods for getting different types of input from the console.
  * @author Grant Peverett
  * @since March 28, 2025
  * @version 1.0 beta
  * @see <a href="https://github.com/Grantyy1/Pet_Store_App_2.0/blob/main/petstore/App/Input.java">GitHub Repository</a>
  */
 public class Input {
-
+    /** Scanner object for reading input from the console */
     public static Scanner sc = new Scanner(System.in);
 
+    /**
+     * Gets a line of text from the user without validation.
+     * @param prompt The message to display to the user
+     * @return The entered text
+     */
     public static String getLine(String prompt) {
         System.out.print(prompt);
         return Input.sc.nextLine();
     } // end of getLine
 
+    /**
+     * Gets a non-empty string from the user with validation.
+     * @param prompt The message to display to the user
+     * @return The validated non-empty string
+     */
     public static String getString(String prompt) {
         String userInput;
 
@@ -38,6 +49,11 @@ public class Input {
         return userInput;
     } // end of getString
 
+    /**
+     * Gets integer value from the user with validation.
+     * @param prompt The message to display to the user.
+     * @return the users input integer.
+     */
     public static int getInt(String prompt) {
         int userInput;
 
@@ -56,6 +72,13 @@ public class Input {
         return userInput;
     } // end of getInt
 
+    /**
+     * Get integer range from the user.
+     * @param prompt The message to display to the user.
+     * @param low lowest integer value.
+     * @param high highest integer value.
+     * @return the integer range inputted by the user.
+     */
     public static int getIntRange(String prompt, int low, int high) {
         int userInput;
 
@@ -82,6 +105,11 @@ public class Input {
         return userInput;
     } // end of getIntRange
 
+    /**
+     * Gets a double from the user with validation.
+     * @param prompt The message to display to the user
+     * @return The validated double
+     */
     public static double getDouble(String prompt) {
         double userInput;
 
@@ -100,6 +128,13 @@ public class Input {
         return userInput;
     } // end of getDouble
 
+    /**
+     * Gets a double from the user within a specified range.
+     * @param prompt The message to display to the user
+     * @param low The minimum acceptable value (inclusive)
+     * @param high The maximum acceptable value (inclusive)
+     * @return The validated double within the specified range
+     */
     public static double getDoubleRange(String prompt, double low, double high) {
         double userInput;
 
@@ -126,6 +161,11 @@ public class Input {
         return userInput;
     } // end of getDoubleRange
 
+    /**
+     * Gets a date string from the user in MM-dd-yyyy format with validation.
+     * @param prompt The message to display to the user
+     * @return The validated date string in MM-dd-yyyy format
+     */
     public static String getDate(String prompt) {
         String userInput;
 
@@ -145,6 +185,13 @@ public class Input {
         return userInput;
     } // end of getDate
 
+    /**
+     * Gets a boolean value from the user using various yes/no inputs.
+     * Accepts "yes", "y", "true", "t", "1" for true and
+     * "no", "n", "false", "f", "0" for false.
+     * @param prompt The message to display to the user
+     * @return The boolean value (true or false)
+     */
     public static boolean getBoolean(String prompt) {
         String userInput;
 
