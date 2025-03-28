5eae68133bf3ff395e298cd58875d67ef5673735
Grant Peverett <grantpeverett@gmail.com>
2025-03-27 19:41:57 -0500

    Java docs added for FeedingSchedule.java

--- petstore/inventory/FeedingSchedule.java
+++ petstore/inventory/FeedingSchedule.java
@@ -1,15 +1,32 @@
 package petstore.inventory;
 /**
- * @author Your Group Member Names Here
- * @since March 26, 2025
+ * Enumeration of possible feeding schedules for pets.
+ * Defines the frequency at which different pets need to be fed.
+ * @author Grant Peverett
+ * @since March 28, 2025
  * @version 1.0 beta
- * @see <a href="https://github.com/yourusername/petstoreapp">GitHub Repository</a>
+ * @see <a href="https://github.com/Grantyy1/Pet_Store_App_2.0/blob/main/petstore/inventory/FeedingSchedule.java">GitHub Repository</a>
  */
 
 public enum FeedingSchedule {
+    /**
+     * Feed once per day
+     */
     ONCE_DAILY,
+    /**
+     * Feed twice per day
+     */
     TWICE_DAILY,
+    /**
+     * Feed three times per day
+     */
     THREE_TIMES_DAILY,
+    /**
+     * Feed once per week
+     */
     WEEKLY,
+    /**
+     * Feed once every two weeks
+     */
     BIWEEKLY,
 }
